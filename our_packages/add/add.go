8a86f54174ec531a50caea5c584e654fe43e3c7b
Stéphane <souellet62@videotron.ca>
2021-03-03 15:50:30 -0500

    Nouveaux fichiers

---
+++ our_packages/add/add.go
@@ -0,0 +1,6 @@
+package add
+
+// Add two integers
+func Add(x int, y int) int {
+	return x + y
+}
--- start.go
+++ start.go
@@ -1,1 +1,13 @@
+package main
 
+import (
+	"fmt"
+	"time"
+	"tutorial2/our_packages/add"
+)
+
+func main() {
+	fmt.Println("Hello Go!")
+	fmt.Println("The time is currently, ", time.Now())
+	fmt.Println("The result is => ", add.Add(8, 8))
+}
